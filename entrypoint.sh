f01bad5caadd848214f6db35c4d0257061f84000
Scott van Looy <scott@ojoho.com>
2020-06-15 17:38:10 +0200

    Update entrypoint.sh
    Tweak config

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ echo "$INPUT_SERVICE_KEY" | base64 --decode > "$HOME"/service_key.json
 
 gcloud auth activate-service-account --key-file="$HOME"/service_key.json --project "$INPUT_PROJECT"
 
-gsutil cp -z css,js,html,svg -r -a "$INPUT_BUILD_FOLDER" gs://"$INPUT_BUCKET_NAME"
+gsutil cp -z "css,js,html,svg" -r -a "$INPUT_BUILD_FOLDER" gs://"$INPUT_BUCKET_NAME"
 gsutil web set -m "$INPUT_HOME_PAGE_PATH" -e "$INPUT_ERROR_PAGE_PATH" gs://"$INPUT_BUCKET_NAME"
