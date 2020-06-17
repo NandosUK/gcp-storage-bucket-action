e360031f67771c473adf61ecd38c1f825fa39a4a
Scott van Looy <scott@ojoho.com>
2020-06-17 17:47:11 +0200

    Update entrypoint.sh
    Hopefully fixing the incorrect copying of the entire subdirectory

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,11 @@ echo "$INPUT_SERVICE_KEY" | base64 --decode > "$HOME"/service_key.json
 
 gcloud auth activate-service-account --key-file="$HOME"/service_key.json --project "$INPUT_PROJECT"
 
-gsutil -m cp -z "css,js,html,svg,json" -r "$INPUT_BUILD_FOLDER/" gs://"$INPUT_BUCKET_NAME"
+cd "$INPUT_BUILD_FOLDER"
+gsutil -m cp -z "css,js,html,svg,json" -r . gs://"$INPUT_BUCKET_NAME"
 #gsutil web set -m "$INPUT_HOME_PAGE_PATH" -e "$INPUT_ERROR_PAGE_PATH" gs://"$INPUT_BUCKET_NAME"
 gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.js
 gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.css
 gsutil -m setmeta -h "Cache-Control:public, max-age=31536000" gs://"$INPUT_BUCKET_NAME"/**/*.svg
-gsutil -m setmeta -h "Cache-Control:public, max-age=20" gs://"$INPUT_BUCKET_NAME"/*.json
-gsutil -m setmeta -h "Cache-Control:public, max-age=20" gs://"$INPUT_BUCKET_NAME"/*.html
+gsutil -m setmeta -h "Cache-Control:public, max-age=20" gs://"$INPUT_BUCKET_NAME"/**/*.json
+gsutil -m setmeta -h "Cache-Control:public, max-age=20" gs://"$INPUT_BUCKET_NAME"/**/*.html
